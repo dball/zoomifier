5f5f70e256764661a5a8b59e11c9da737306859c
Donald Ball <donald.ball@gmail.com>
2008-11-21 00:32:01 -0600

    testing that an actual jpeg is zoomified

--- spec/zoomifier_spec.rb
+++ spec/zoomifier_spec.rb
@@ -5,4 +5,15 @@ describe Zoomifier do
   it "should respond to its main method" do
     Zoomifier.should respond_to(:zoomify)
   end
+
+  describe "1024x768.jpg" do
+    it "should zoomify" do
+      output = File.dirname(__FILE__) + '/data/1024x768/'
+      FileUtils.rm_rf(output)
+      input = File.dirname(__FILE__) + '/data/1024x768.jpg'
+      Zoomifier::zoomify(input)
+      File.directory?(output).should be_true
+      FileUtils.rm_rf(output)
+    end
+  end
 end
